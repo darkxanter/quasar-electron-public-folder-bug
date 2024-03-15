230bc70a22c3ecd71c7ffb5494ede7e849f2cb15
Sergey Shumov <sergey0001@gmail.com>
2024-03-15 17:04:16 +0300

    initial

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <img src="/icons/favicon-128x128.png" />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+});
+</script>
